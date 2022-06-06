47d290f5e9f9bbfbba9fe8476cd30a958e98aaf5
condac <burns.nu@gmail.com>
2022-06-06 11:06:18 +0200

    filterfunktion

--- BevattningLCD/BevattningLCD.ino
+++ BevattningLCD/BevattningLCD.ino
@@ -11,21 +11,112 @@ float t,h;
 
 #define SENSOR1_PIN A1
 #define SENSOR2_PIN A2
+#define SENSOR3_PIN A3
+#define SENSOR4_PIN A4
 //#define SENSOR1_AIR 540
 //#define SENSOR1_WATER 210
 
 #define PUMP1_PIN 11
 #define PUMP2_PIN 12
+#define PUMP3_PIN 13
+#define PUMP4_PIN 3
 //#define PUMP1_TRIGGER 30
 
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value output to the PWM (analog out)
 
-bool pump1_state;
-int sensor1_value;
-int SENSOR1_AIR = 540;
-int SENSOR1_WATER = 210;
-byte PUMP1_TRIGGER = 30;
+#define WIFI_RESET_PIN 22
+
+//int sensorValue = 0;        // value read from the pot
+//int sensorValue2 = 0;        // value read from the pot
+//int outputValue = 0;        // value output to the PWM (analog out)
+
+//bool pump_state[2];
+//bool pump2_state;
+//bool pump3_state;
+//bool pump4_state;
+//int sensor1_value;
+//int sensor2_value;
+//int sensor3_value;
+//int sensor4_value;
+//int SENSOR1_AIR = 540;
+//int SENSOR_AIR[2] = 540;
+//int SENSOR3_AIR = 540;
+//int SENSOR4_AIR = 540;
+//int SENSOR1_WATER = 210;
+//int SENSOR_WATER2] = 210;
+//int SENSOR3_WATER = 210;
+//int SENSOR4_WATER = 210;
+//byte PUMP1_TRIGGER = 30;
+//byte PUMP_TRIGGER[2] = 30;
+//byte PUMP3_TRIGGER = 30;
+//byte PUMP4_TRIGGER = 30;
+
+bool pump_state[5];
+int sensor_value[5];
+int fukt[5];
+int SENSOR_AIR[5] = {210,540,210,30,30};
+int SENSOR_WATER[5] = {210,210,210,30,30};
+byte PUMP_TRIGGER[5] = {30,30,30,30,30};
+bool failstate[5];
+long timerpump[5];
+long timerfilter[5];
+
+int PUMP_PIN[5] = {0,PUMP1_PIN,PUMP2_PIN,PUMP3_PIN,PUMP4_PIN};
+int SENSOR_PIN[5] = {0,SENSOR1_PIN,SENSOR2_PIN,SENSOR3_PIN,SENSOR4_PIN};
+
+void setupSavedata() {
+  byte data = EEPROM.read(1);
+  if (data == 10) {
+    Serial.println("Found savedata");
+
+    SENSOR_AIR[1] = EEPROM.read(2)*4;
+    SENSOR_WATER[1] = EEPROM.read(3)*4;
+    PUMP_TRIGGER[1] = EEPROM.read(4);
+
+    Serial.print("SENSOR1_AIR");
+    Serial.println(SENSOR_AIR[1]);
+    Serial.print("SENSOR1_WATER");
+    Serial.println(SENSOR_WATER[1]);
+    Serial.print("PUMP1_TRIGGER");
+    Serial.println(PUMP_TRIGGER[1]);
+
+    SENSOR_AIR[2] = EEPROM.read(5)*4;
+    SENSOR_WATER[2] = EEPROM.read(6)*4;
+    PUMP_TRIGGER[2] = EEPROM.read(7);
+
+    Serial.print("SENSOR_AIR[2]");
+    Serial.println(SENSOR_AIR[2]);
+    Serial.print("SENSOR_WATER2]");
+    Serial.println(SENSOR_WATER[2]);
+    Serial.print("PUMP_TRIGGER[2]");
+    Serial.println(PUMP_TRIGGER[2]);
+    
+  } else {
+    Serial.print("Invalid savedata");
+    Serial.println(data);
+    
+    EEPROM.write(1, 10);
+    EEPROM.write(2, SENSOR_AIR[1]/4);
+    EEPROM.write(3, SENSOR_WATER[1]/4);
+    EEPROM.write(4, PUMP_TRIGGER[1]);
+    EEPROM.write(5, SENSOR_AIR[2]/4);
+    EEPROM.write(6, SENSOR_WATER[2]/4);
+    EEPROM.write(7, PUMP_TRIGGER[2]);
+  }
+  //EEPROM.write(address, value);
+}
+
+void writeSaveData() {
+  Serial.println("Write savedata");
+  
+  
+  EEPROM.write(1, 10);
+  EEPROM.write(2, SENSOR_AIR[1]/4);
+  EEPROM.write(3, SENSOR_WATER[1]/4);
+  EEPROM.write(4, PUMP_TRIGGER[1]);
+  EEPROM.write(5, SENSOR_AIR[2]/4);
+  EEPROM.write(6, SENSOR_WATER[2]/4);
+  EEPROM.write(7, PUMP_TRIGGER[2]);
+}
 
 #include "buttons.h"
 #include "lcd.h"
@@ -35,6 +126,10 @@ void setup() {
   pinMode(PUMP2_PIN, OUTPUT);
   digitalWrite(PUMP1_PIN,LOW);
   digitalWrite(PUMP2_PIN,LOW);
+
+  pinMode(WIFI_RESET_PIN, OUTPUT);
+  digitalWrite(WIFI_RESET_PIN,HIGH);
+  
   
   initLCD();
   Serial.begin(9600);
@@ -48,70 +143,126 @@ void loop() {
   btn_loop();
   readDHT();
   drawMenu();
-  // read the analog in value:
-  sensorValue = analogRead(SENSOR1_PIN);
-  // map it to the range of the analog out:
-  outputValue = map(sensorValue, SENSOR1_AIR, SENSOR1_WATER, 0, 100);
-  sensor1_value = constrain(outputValue, 0, 100);
-  // change the analog out value:
+
+  nypump(1);
+  delay(100); 
+  nypump(2);
+
+  resetWifi();
+//  sensorValue = analogRead(SENSOR1_PIN);
+//  outputValue = map(sensorValue, SENSOR1_AIR, SENSOR1_WATER, 0, 100);
+//  sensor1_value = constrain(outputValue, 0, 100);
+//
+//  pump(PUMP1_PIN, PUMP1_TRIGGER, &pump_state[2]);
+//  failsafe(PUMP1_PIN, sensorValue, SENSOR1_AIR, SENSOR1_WATER, &pump_state[2], 1);
+//  delay(100); 
+//   
+//  sensorValue2 = analogRead(SENSOR2_PIN);
+//  outputValue = map(sensorValue2, SENSOR_AIR[2], SENSOR_WATER2], 0, 100);
+//  sensor2_value = constrain(outputValue, 0, 100);  
+//  
+//  pump(PUMP2_PIN, PUMP_TRIGGER[2], &pump2_state);
+//  failsafe(PUMP2_PIN, sensorValue2, SENSOR_AIR[2], SENSOR_WATER2], &pump2_state, 2);
+
+ 
 
 //  if (outputValue<PUMP1_TRIGGER) {
 //    digitalWrite(PUMP1_PIN,HIGH);
-//    pump1_state = true;
+//    pump_state[2] = true;
 //  }else {
 //    digitalWrite(PUMP1_PIN,LOW);
-//    pump1_state = false;
+//    pump_state[2] = false;
 //  }
-  pump(PUMP1_PIN, PUMP1_TRIGGER, &pump1_state);
+
     // wait 2 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
   sendData();
-  delay(2);
+
 }
 
-long timerpump;
-void pump(int pin, int trigger, bool* state) {
-  if (millis()>timerpump ) {
-    timerpump = millis()+1000;
+void resetWifi() {
+  static long resettimer = 0;
+  if (millis()>resettimer ) {
+    resettimer = millis()+(1000*60*14);
+    digitalWrite(WIFI_RESET_PIN,LOW);
+    delay(200);
+    digitalWrite(WIFI_RESET_PIN,HIGH);
   }else {
     return;
   }
-  if (outputValue<trigger) {
-    digitalWrite(pin,HIGH);
-    *state = true;
-  }else {
+  
+  
+}
+
+//void pump(int pin, int trigger, bool* state) {
+//  if (millis()>timerpump ) {
+//    timerpump = millis()+1000;
+//  }else {
+//    return;
+//  }
+//  if (outputValue<trigger) {
+//    digitalWrite(pin,HIGH);
+//    *state = true;
+//  }else {
+//    digitalWrite(pin,LOW);
+//    *state = false;
+//  }
+//}
+
+void failsafe(int pin, int value, int maxval, int minval, bool* state, int nr) {
+
+  if (value>maxval || value<minval) {
     digitalWrite(pin,LOW);
     *state = false;
+    failstate[nr] = true;
+  }else {
+    
+    failstate[nr] = false;
   }
 }
 
-
-void setupSavedata() {
-  byte data = EEPROM.read(1);
-  if (data == 10) {
-    Serial.println("Found savedata");
-
-    SENSOR1_AIR = EEPROM.read(2)*4;
-    SENSOR1_WATER = EEPROM.read(3)*4;
-    PUMP1_TRIGGER = EEPROM.read(4);
-
-    Serial.print("SENSOR1_AIR");
-    Serial.println(SENSOR1_AIR);
-    Serial.print("SENSOR1_WATER");
-    Serial.println(SENSOR1_WATER);
-    Serial.print("PUMP1_TRIGGER");
-    Serial.println(PUMP1_TRIGGER);
-    
-  } else {
-    Serial.print("Invalid savedata");
-    Serial.println(data);
-    
-    EEPROM.write(1, 10);
-    EEPROM.write(2, SENSOR1_AIR/4);
-    EEPROM.write(3, SENSOR1_WATER/4);
-    EEPROM.write(4, PUMP1_TRIGGER);
+void nypump(int nr) {
+  
+  if (millis()>timerpump[nr] ) {
+    timerpump[nr] = millis()+1000;
+  }else {
+    return;
   }
-  //EEPROM.write(address, value);
+
+  sensor_value[nr] = analogRead(SENSOR_PIN[nr]);
+  int oV = map(sensor_value[nr], SENSOR_AIR[nr], SENSOR_WATER[nr], 0, 100);
+  fukt[nr] = constrain(oV, 0, 100);
+
+  // FAILSAFE
+  if (sensor_value[nr]>SENSOR_AIR[nr] || sensor_value[nr]<SENSOR_WATER[nr]) {
+    digitalWrite(PUMP_PIN[nr],LOW);
+    pump_state[nr] = false;
+    failstate[nr] = true;
+    timerfilter[nr] = 0;
+    return;
+  }else {
+    failstate[nr] = false;
+  }
+
+  if (fukt[nr]<PUMP_TRIGGER[nr]) {
+    if (filterFunction(nr) == 1) {
+      digitalWrite(PUMP_PIN[nr],HIGH);
+      pump_state[nr] = true;
+    }
+    
+  }else {
+    timerfilter[nr] = 0;
+    digitalWrite(PUMP_PIN[nr],LOW);
+    pump_state[nr] = false;
+  }
+}
+
+int filterFunction(int nr) {
+  timerfilter[nr] = timerfilter[nr]+1; // anroppas en gång per sekund
+  if (timerfilter[nr]>(60*5) ) { // fuktmätaren måste vara inom godtyckligt triggervärde i 5 minuter innan pumpen aktiveras
+    return 1;
+  }
+  return 0;
 }
 
 void sendData() {
@@ -122,11 +273,11 @@ void sendData() {
     return;
   }
   Serial.print("{\"s1\":");
-  Serial.print(sensor1_value);
+  Serial.print(fukt[1]);
   Serial.println("}");
 
   Serial.print("{\"p1\":");
-  Serial.print(pump1_state);
+  Serial.print(pump_state[1]);
   Serial.println("}");
 
   Serial.print("{\"t1\":");
@@ -137,8 +288,6 @@ void sendData() {
   Serial.print(h);
   Serial.println("}");
  
-  
-  
 }
 
 void readDHT() {
--- BevattningLCD/lcd.h
+++ BevattningLCD/lcd.h
@@ -4,7 +4,8 @@
 #define MENU_IDLE     2
 #define MENU_SELECT   10
 #define MENU_EDIT_SENSOR     20
-#define MENU_WRITE_FLASH     30
+#define MENU_EDIT_TARGET     30
+#define MENU_WRITE_FLASH     40
 
 int currentMenu = MENU_IDLE;
 int prevMenu = 1;
@@ -38,7 +39,10 @@ void menuMain() {
     if (page == 0 && curline == 1) { // Edit sensor
       currentMenu = MENU_EDIT_SENSOR;
     }
-    if (page == 1 && curline == 0) { // Write flash
+    if (page == 1 && curline == 0) { // Edit target
+      currentMenu = MENU_EDIT_TARGET;
+    }
+    if (page == 1 && curline == 1) { // Write flash
       currentMenu = MENU_WRITE_FLASH;
     }
     
@@ -86,17 +90,14 @@ void menuMain() {
   if (page == 1) {
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("  Write flash");
+    lcd.print("  Edit Target");
     lcd.setCursor(0,1);
-    lcd.print("  ");
+    lcd.print("  Write flash");
   }
   
   lcd.setCursor(0,curline);
   lcd.print(">");
-  lcd.setCursor(12,0);
-  lcd.print("4");
 
-  lcd.setCursor(0,1);
  
   
   
@@ -109,22 +110,37 @@ void menuIdle() {
   }
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print(sensor1_value);
+  lcd.print(fukt[1]);
   lcd.print("%");
   lcd.setCursor(4,0);
-  lcd.print("2");
+  lcd.print(fukt[2]);
+  lcd.print("%");
   lcd.setCursor(8,0);
   lcd.print("3");
   lcd.setCursor(12,0);
   lcd.print("4");
 
   lcd.setCursor(0,1);
-  if (pump1_state) {
-    lcd.print("On");
+  if (failstate[1]) {
+    lcd.print("fail");
   }else {
-    lcd.print("Off");
+    if (pump_state[1]) {
+      lcd.print("On");
+    }else {
+      lcd.print("Off");
+    }
   }
-
+  
+  lcd.setCursor(4,1);
+  if (failstate[2]) {
+    lcd.print("fail");
+  }else {
+    if (pump_state[2]) {
+      lcd.print("On");
+    }else {
+      lcd.print("Off");
+    }
+  }
   if ( btnPressed == btnSELECT) {
     btnPressed = 0;
     currentMenu = MENU_MAIN;
@@ -163,6 +179,7 @@ void MenuSelect() {
 
 void MenuEditSensor() {
   static byte cur = 0;
+  static byte sen = 1;
   if (millis()>redraw || btnPressed != 0) {
     redraw = millis()+500;
   }else {
@@ -183,56 +200,223 @@ void MenuEditSensor() {
     btnPressed = 0;
     cur++;
   }
-  cur = constrain(cur, 0,1);
+  cur = constrain(cur, 0,2);
+  sen = constrain(sen, 1,4);
 
   if ( btnPressed == btnRIGHT) {
     btnPressed = 0;
     if (cur == 0) {
-      SENSOR1_AIR = SENSOR1_AIR +4;
+      sen++;
+      sen = constrain(sen, 1,4);
     }
-    if (cur == 1) {
-      SENSOR1_WATER = SENSOR1_WATER +4;
+    if (sen == 1) {
+      if (cur == 1) {
+        SENSOR_AIR[1] = SENSOR_AIR[1] +4;
+        SENSOR_AIR[1] = constrain(SENSOR_AIR[1], 0,1023);
+      }
+      if (cur == 2) {
+        SENSOR_WATER[1] = SENSOR_WATER[1] +4;
+        SENSOR_WATER[1] = constrain(SENSOR_WATER[1], 0,1023);
+      }
+    }
+    if (sen == 2) {
+      if (cur == 1) {
+        SENSOR_AIR[2] = SENSOR_AIR[2] +4;
+        SENSOR_AIR[2] = constrain(SENSOR_AIR[2], 0,1023);
+      }
+      if (cur == 2) {
+        SENSOR_WATER[2] = SENSOR_WATER[2] +4;
+        SENSOR_WATER[2] = constrain(SENSOR_WATER[2], 0,1023);
+      }
     }
   }
   if ( btnPressed == btnLEFT) {
     btnPressed = 0;
     if (cur == 0) {
-      SENSOR1_AIR = SENSOR1_AIR -4;
+      sen--;
+      sen = constrain(sen, 1,4);
     }
-    if (cur == 1) {
-      SENSOR1_WATER = SENSOR1_WATER -4;
+    if (sen == 1) {
+      if (cur == 1) {
+        SENSOR_AIR[1] = SENSOR_AIR[1] -4;
+        SENSOR_AIR[1] = constrain(SENSOR_AIR[1], 0,1023);
+      }
+      if (cur == 2) {
+        SENSOR_WATER[1] = SENSOR_WATER[1] -4;
+        SENSOR_WATER[1] = constrain(SENSOR_WATER[1], 0,1023);
+      }
+    }
+    if (sen == 2) {
+      if (cur == 1) {
+        SENSOR_AIR[2] = SENSOR_AIR[2] -4;
+        SENSOR_AIR[2] = constrain(SENSOR_AIR[2], 0,1023);
+      }
+      if (cur == 2) {
+        SENSOR_WATER[2] = SENSOR_WATER[2] -4;
+        SENSOR_WATER[2] = constrain(SENSOR_WATER[2], 0,1023);
+      }
     }
   }
 
-  lcd.setCursor(0,0);
-  lcd.print("s1:");
-  lcd.print(sensor1_value);
-  lcd.print("%");
-  lcd.setCursor(8,0);
-  lcd.print("dry:");
-  lcd.print(SENSOR1_AIR);
+  if (sen == 1) {
+    lcd.setCursor(0,0);
+    lcd.print("s1:");
+    lcd.print(fukt[1]);
+    lcd.print("%");
+    lcd.setCursor(8,0);
+    lcd.print("dry:");
+    lcd.print(SENSOR_AIR[1]);
+  
+    lcd.setCursor(0,1);
+    lcd.print(sensor_value[1]);
+    lcd.setCursor(8,1);
+    lcd.print("wet:");
+    lcd.print(SENSOR_WATER[1]);
+  }
+  if (sen == 2) {
+    lcd.setCursor(0,0);
+    lcd.print("s2:");
+    lcd.print(fukt[2]);
+    lcd.print("%");
+    lcd.setCursor(8,0);
+    lcd.print("dry:");
+    lcd.print(SENSOR_AIR[2]);
+  
+    lcd.setCursor(0,1);
+    lcd.print(sensor_value[2]);
+    lcd.setCursor(8,1);
+    lcd.print("wet:");
+    lcd.print(SENSOR_WATER[2]);
+  }
+  
 
-  lcd.setCursor(0,1);
-  lcd.print(sensorValue);
-  lcd.setCursor(8,1);
-  lcd.print("wet:");
-  lcd.print(SENSOR1_WATER);
+  if (cur >0) {
+    lcd.setCursor(7,cur);
+    lcd.print(">");
+  }else {
+    lcd.setCursor(0,0);
+    lcd.print(">");
+  }
+  
+}
 
-  lcd.setCursor(7,cur);
+void MenuEditTarget() {
+  static byte cur = 0;
+  if (millis()>redraw || btnPressed != 0) {
+    redraw = millis()+500;
+  }else {
+    return;
+  }
+  lcd.clear();
+  if ( btnPressed == btnSELECT) {
+    btnPressed = 0;
+    currentMenu = MENU_MAIN;
+    return;
+  }
+  if ( btnPressed == btnLEFT) {
+    btnPressed = 0;
+    cur--;
+  }
+  if ( btnPressed == btnRIGHT) {
+    btnPressed = 0;
+    cur++;
+  }
+  cur = constrain(cur, 0,3);
+
+  if ( btnPressed == btnUP) {
+    btnPressed = 0;
+    if (cur == 0) {
+      PUMP_TRIGGER[1] = PUMP_TRIGGER[1] +1;
+      PUMP_TRIGGER[1] = constrain(PUMP_TRIGGER[1], 0,100);
+    }
+    if (cur == 1) {
+      PUMP_TRIGGER[2] = PUMP_TRIGGER[2] +1;
+      PUMP_TRIGGER[2] = constrain(PUMP_TRIGGER[2], 0,100);
+    }
+    
+  }
+  if ( btnPressed == btnDOWN) {
+    btnPressed = 0;
+    if (cur == 0) {
+      PUMP_TRIGGER[1] = PUMP_TRIGGER[1] -1;
+      PUMP_TRIGGER[1] = constrain(PUMP_TRIGGER[1], 0,100);
+    }
+    if (cur == 1) {
+      PUMP_TRIGGER[2] = PUMP_TRIGGER[2] -1;
+      PUMP_TRIGGER[2] = constrain(PUMP_TRIGGER[2], 0,100);
+    }
+    
+  }
+
+
+  lcd.setCursor(1,0);
+  lcd.print("1:");
+  lcd.print(fukt[1]);
+  lcd.setCursor(5,0);
+  lcd.print("2:");
+  lcd.print(fukt[2]);
+  lcd.setCursor(9,0);
+  lcd.print("3:");
+  lcd.print(fukt[3]);
+  lcd.setCursor(13,0);
+  lcd.print("4:");
+  lcd.print(fukt[4]);
+
+  lcd.setCursor(1,1);
+  
+  lcd.print(PUMP_TRIGGER[1]);
+  lcd.setCursor(5,2);
+  lcd.print(PUMP_TRIGGER[2]);
+  lcd.setCursor(9,3);
+  lcd.print(PUMP_TRIGGER[3]);
+  lcd.setCursor(13,4);
+  lcd.print(PUMP_TRIGGER[4]);
+  
+
+  lcd.setCursor(4*cur,0);
   lcd.print(">");
 }
 
 void MenuWriteFlash() {
-  if ( btnPressed == btnSELECT) {
-    btnPressed = 0;
-    currentMenu = MENU_MAIN;
-    lcd.clear();
+  static byte cur = 0;
+  if (millis()>redraw || btnPressed != 0) {
+    redraw = millis()+500;
+  }else {
     return;
   }
+  lcd.clear();
+  if ( btnPressed == btnSELECT) {
+    btnPressed = 0;
+    if (cur ==1) {
+      writeSaveData();
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(" Save complete");
+      delay(1000);
+    }
+    currentMenu = MENU_MAIN;
+    
+    return;
+  }
+  if ( btnPressed == btnDOWN) {
+    btnPressed = 0;
+    cur--;
+  }
+  if ( btnPressed == btnUP) {
+    btnPressed = 0;
+    cur++;
+  }
+  cur = constrain(cur, 0,1);
 
-  lcd.setCursor(0,0);
-  lcd.print("Write Flash:");
 
+  lcd.setCursor(1,0);
+  lcd.print(" Back");
+
+  lcd.setCursor(1,1);
+  lcd.print(" Save");
+
+  lcd.setCursor(0,cur);
+  lcd.print(">");
 }
 
 
@@ -254,6 +438,10 @@ void drawMenu() {
     }
     case MENU_EDIT_SENSOR: {
         MenuEditSensor();
+        break;
+    }
+    case MENU_EDIT_TARGET: {
+        MenuEditTarget();
         break;
     }
     case MENU_WRITE_FLASH: {
